be4e8ba96c5367271e9123e38fff22ead57d4c90
Shreyansh Tripathi <42905282+shreyansh02@users.noreply.github.com>
2024-06-07 01:26:22 +0530

    Create firebaseConfig.js

---
+++ frontend/src/firebaseConfig.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBfCBvTBydO7aK4WEeyly9XydrbSLScEDs",
+  authDomain: "mock-project-58e20.firebaseapp.com",
+  projectId: "mock-project-58e20",
+  storageBucket: "mock-project-58e20.appspot.com",
+  messagingSenderId: "848264271100",
+  appId: "1:848264271100:web:b1ce0d908dea5896a27379",
+  measurementId: "G-JD1H7N24XV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+ export const auth = getAuth(app);
