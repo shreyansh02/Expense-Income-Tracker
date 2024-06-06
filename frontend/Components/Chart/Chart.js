ed53eb269c7e0eb517c167ef2da12b857a81543f
Shreyansh Tripathi <42905282+shreyansh02@users.noreply.github.com>
2024-06-07 00:55:05 +0530

    Create Chart.js

---
+++ frontend/Components/Chart/Chart.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Chart as ChartJs, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, ArcElement } from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import styled from 'styled-components';
+import { useGlobalContext } from '../../context/globalContext';
+import { dateFormat } from '../../utils/dateFormat';
+
+ChartJs.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, ArcElement);
+
+function Chart() {
+  const { incomes, expenses } = useGlobalContext();
+
+  const [selectedData, setSelectedData] = React.useState('both');
+
+  const data = {
+    labels: incomes.map((inc) => dateFormat(inc.date)),
+    datasets: [
+      {
+        label: 'Income',
+        data: incomes.map((income) => income.amount),
+        backgroundColor: 'green',
+        tension: 0.2,
+        hidden: selectedData === 'expenses',
+      },
+      {
+        label: 'Expenses',
+        data: expenses.map((expense) => expense.amount),
+        backgroundColor: 'red',
+        tension: 0.2,
+        hidden: selectedData === 'incomes',
+      },
+    ],
+  };
+
+  const handleSelectChange = (event) => {
+    setSelectedData(event.target.value);
+  };
+
+  return (
+    <ChartStyled>
+      <select value={selectedData} onChange={handleSelectChange}>
+        <option value="both">Both</option>
+        <option value="incomes">Incomes</option>
+        <option value="expenses">Expenses</option>
+      </select>
+      <Line data={data} />
+    </ChartStyled>
+  );
+}
+
+const ChartStyled = styled.div`
+  background: #FCF6F9;
+  border: 2px solid #FFFFFF;
+  box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+  padding: 1rem;
+  border-radius: 20px;
+  height: 100%;
+`;
+
+export default Chart;
